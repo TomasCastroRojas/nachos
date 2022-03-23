3b3ca7a9a351f2fce8d8085e753779546114efb0
TomasCastro <tomascastrorojas18@gmail.com>
2022-03-23 10:15:29 -0300

    [threads]: Ejercicio 14, 5 hilos de prueba

--- code/threads/thread_test_simple.cc
+++ code/threads/thread_test_simple.cc
@@ -39,10 +39,25 @@ SimpleThread(void *name_)
 void
 ThreadTestSimple()
 {
-    char *name = new char [64];
-    strncpy(name, "2nd", 64);
-    Thread *newThread = new Thread(name);
-    newThread->Fork(SimpleThread, (void *) name);
+    char *name2 = new char[64];
+    strncpy(name2, "2nd", 64);
+    Thread *newThread = new Thread(name2);
+    newThread->Fork(SimpleThread, (void *) name2);
+
+    char *name3 = new char[64];
+    strncpy(name3, "3nd", 64);
+    Thread *newThread = new Thread(name3);
+    newThread->Fork(SimpleThread, (void *) name3);
+
+    char *name4 = new char[64];
+    strncpy(name4, "4nd", 64);
+    Thread *newThread = new Thread(name4);
+    newThread->Fork(SimpleThread, (void *) name4);
+
+    char *name5 = new char[64];
+    strncpy(name5, "5nd", 64);
+    Thread *newThread = new Thread(name5);
+    newThread->Fork(SimpleThread, (void *) name5);
 
     SimpleThread((void *) "1st");
 }
