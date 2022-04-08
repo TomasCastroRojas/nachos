f536f44d9520cd69e14e008ab5621d316afe0625
TomasCastro <tomascastrorojas18@gmail.com>
2022-04-08 14:38:40 -0300

    [threads]: problema de productores y consumidores. mensajes de debug para continuar testeando. si hay mas consumidores se rompe

--- code/threads/thread_test_prod_cons.cc
+++ code/threads/thread_test_prod_cons.cc
@@ -10,65 +10,95 @@
 #include "system.hh"
 
 #include <stdio.h>
-#include <string.h>
 
-static const int items = 10;
+static const int CICLOS = 5;
+static const int CAPACIDAD = 10;
 int buffer = 0;
-static const int N_PROD_CONS = 2;
+static const int N_PROD = 2;
+static const int N_CONS = 2;
+static const int N_PROD_CONS = N_PROD + N_CONS;
 bool done[N_PROD_CONS];
+
 
 Lock *lock = new Lock("lockProdCons");
 Condition *condFull = new Condition("condition full", lock);
 Condition *condEmpty = new Condition("condition empty", lock);
 
-void producer (void* arg)
+static void producer (void* arg)
 {
-    printf("%s producer arrancando\n", currentThread->GetName());
-    while (1) {
+    printf("[PRODUCER]: %s arrancando\n", currentThread->GetName());
+    for (int i=0; i < CICLOS; i++) 
+    {
         lock->Acquire();
-        while (buffer == items) {
-            printf ("%s producer espera\n", currentThread->GetName());
+        while (buffer == CAPACIDAD) 
+        {
+            printf ("[PRODUCER]: %s espera\n", currentThread->GetName());
             condFull->Wait();
         }
-        printf("%s produciendo\n", currentThread->GetName());
+        printf("[PRODUCER]: %s produciendo\n", currentThread->GetName());
         buffer++;
         condEmpty->Signal();
+        printf("[PRODUCER]: %s -- buffer = %d\n",currentThread->GetName(), buffer);
         lock->Release();
+        printf("[PRODUCER]: %s after release\n",currentThread->GetName());
         currentThread->Yield();
     }
+    int id = *(int*)arg;
+    printf("[PRODUCER DONE]: productor %u\n", id);
+    done[id] = true;
 }
 
-void consumer (void* arg)
+static void consumer (void* arg)
 {
-    printf("%s consumer arrancando\n", currentThread->GetName());
-    while (1) {
+    printf("[CONSUMER]: %s arrancando\n", currentThread->GetName());
+    for (int i=0; i < CICLOS; i++)
+    {
         lock->Acquire();
-        while (buffer == 0) {
-            printf ("%s consumer espera\n", currentThread->GetName());
+        while (buffer == 0) 
+        {
+            printf ("[CONSUMER]: %s espera\n", currentThread->GetName());
             condEmpty->Wait();
         }
-        printf("%s consumiendo\n", currentThread->GetName());
+        printf("[CONSUMER]: %s consumiendo\n", currentThread->GetName());
         buffer--;
         condFull->Signal();
+        printf("[CONSUMER]: %s -- buffer = %d\n",currentThread->GetName(), buffer);
         lock->Release();
         currentThread->Yield();
     }
+    int id = *(int*)arg;
+    printf("[CONSUMER DONE]: consumidor %u\n", id);
+    done[id] = true;
 }
 
 void
 ThreadTestProdCons()
 {
-    Thread *tProd = new Thread("hilo prod");
-    done[0] = false;
-    tProd->Fork(producer, nullptr);
     
+    for (int i = 0; i < N_CONS; ++i) 
+    {
+        char *name = new char[64];
+        sprintf(name, "Consumidor %u", i);
+        int* n = new int;
+        *n = i;
+        Thread *tCons = new Thread(name);
+        tCons->Fork(consumer, (void*)n);
+    }
+    for (int i = 0; i < N_PROD; ++i) 
+    {
+        char *name = new char[64];
+        sprintf(name, "Productor %u", i);
+        int* n = new int;
+        *n = i + N_CONS;
+        Thread *tProd = new Thread(name);
+        tProd->Fork(producer, (void*)n);
+    }
 
-    Thread *tCons = new Thread("hilo cons");
-    done[1] = false;
-    tCons->Fork(consumer, nullptr);
 
     for (unsigned i = 0; i < N_PROD_CONS; i++) {
+        printf("[HARDCORE JOIN]: %u\n", i);
         while (!done[i]) {
+            printf("[NOT DONE]: hilo %u\n", i);
             currentThread->Yield();
         }
     }
