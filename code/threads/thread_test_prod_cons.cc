91220b9254636e5a741ac26b12a798f9b9816dbc
TomasCastro <tomascastrorojas18@gmail.com>
2022-04-08 10:32:10 -0300

    [threads]: esqueleto para prod-cons, no entiendo como implementarlo sin Join

--- code/threads/thread_test_prod_cons.cc
+++ code/threads/thread_test_prod_cons.cc
@@ -6,12 +6,57 @@
 
 
 #include "thread_test_prod_cons.hh"
+#include "condition.hh"
+#include "system.hh"
 
 #include <stdio.h>
+#include <string.h>
 
+static const int items = 10;
+int buffer = 0;
+
+Lock *lock = new Lock("lockProdCons");
+Condition *condFull = new Condition("condition full", lock);
+Condition *condEmpty = new Condition("condition empty", lock);
+
+void producer (void* arg)
+{
+    printf("%s producer arrancando\n", currentThread->GetName());
+    while (1) {
+        lock->Acquire();
+        while (buffer == items) {
+            condFull->Wait();
+        }
+        printf("%s produciendo\n", currentThread->GetName());
+        buffer++;
+        condEmpty->Signal();
+        lock->Release();
+        currentThread->Yield();
+    }
+}
+
+void consumer (void* arg)
+{
+    printf("%s consumer arrancando\n", currentThread->GetName());
+    while (1) {
+        lock->Acquire();
+        while (buffer == 0) {
+            condEmpty->Wait();
+        }
+        printf("%s consumiendo\n", currentThread->GetName());
+        buffer--;
+        condFull->Signal();
+        lock->Release();
+        currentThread->Yield();
+    }
+}
 
 void
 ThreadTestProdCons()
 {
-    printf("Test unimplemented!\n");
+    Thread *tProd = new Thread("hilo prod");
+    tProd->Fork(producer, nullptr);
+
+    Thread *tCons = new Thread("hilo cons");
+    tCons->Fork(consumer, nullptr);
 }
