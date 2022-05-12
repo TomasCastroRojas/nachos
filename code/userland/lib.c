47c0d04da30881ad9db68db96ceaf5c51eea9ff7
TomasCastro <tomascastrorojas18@gmail.com>
2022-05-12 11:51:10 -0300

    [lib]: implementacion de itoa y renombre de funciones para evitar conflictos con C

--- code/userland/lib.c
+++ code/userland/lib.c
@@ -1,6 +1,6 @@
 #include "syscall.h"
 
-unsigned strlen (const char *string) {
+unsigned strlargo (const char *string) {
   unsigned len = 0;
   
   for(; string[len] != '\0'; ++len);
@@ -8,11 +8,45 @@ unsigned strlen (const char *string) {
   return len;
 }
 
-void puts (const char *string ) {
-  Write(string, strlen(string), CONSOLE_OUTPUT);
-  Write("\n", 1, CONSOLE_OUTPUT);
+void putS (const char *string ) {
+  Write(string, strlargo(string), CONSOLE_OUTPUT);
 }
 
-void itoa (int n, char *string) {
+ 
+// Function to reverse a string
+void reverse_string(char *str, unsigned len) {
+    char temp;
+    for(unsigned i = 0; i < (len / 2); i++) {
+        temp = str[i];
+        str[i] = str[len - i - 1];
+        str[len - i - 1] = temp;
+    }
+}
 
+void itoa (int n, char *str) {
+    int negative = 0;
+    if (n < 0) {
+        n *= -1;
+        negative = 1;
+    }    
+    int i = 0, r;
+    while (n)
+    {
+        r = n % 10;
+        
+        str[i++] = 48 + r;
+ 
+        n = n / 10;
+    }
+    // if the number is 0
+    if (i == 0) {
+        str[i++] = '0';
+    }
+
+    if (negative)
+        str[i++] = '-';
+    
+    str[i] = '\0';
+
+    reverse_string(str, i);
 }
